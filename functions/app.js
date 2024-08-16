69db5471b872ae735ca606a22eb79c4ff8edc984
unknown <singamajaygoud1999@gmail.com>
2024-08-16 17:57:17 -0400

    netlify deploy

--- functions/app.js
+++ functions/app.js
@@ -1,13 +1,4 @@
-//app.js
+const serverless = require('serverless-http');
+const app = require('../app');
 
-const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send("App is running..");
-});
-
-app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
