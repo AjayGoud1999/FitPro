e8678497192c3012cc2f7dd9a4d1d9ff6b750581
unknown <singamajaygoud1999@gmail.com>
2024-08-16 23:51:39 -0400

    updated browser list db

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Exercise from "./components/exercise.js";
 import ExerciseCard from "./components/exerciseCard.js";
 import Login from "./components/login.js";
-
+import MainSection from "./components/mainSection.js";
 function App() {
   return (
     <div className="App">
